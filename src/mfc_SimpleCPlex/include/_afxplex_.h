f3cfe4f1c3d8f0cb702d8708deee9e0543a3abda
handabao <18862963156@163.com>
2025-11-21 14:51:57 +0800

    内存池链式数据结构，专门用于高效管理批量小对象的内存分配。 // 1. 减少内存分配次数 // 一次性分配 nMax 个元素，而不是每次Add都分配
    // 2. 避免数据搬迁
    // 新块直接链入，原有数据不动
    
    // 3. 局部性原理
    // 块内数据连续，缓存友好

---
+++ src/mfc_SimpleCPlex/include/_afxplex_.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <windows.h>
+
+struct CPlex
+{
+    CPlex* pNext; /// 向我们组织的内存块，用链表串联的指针
+
+    /// 数据区
+    void* data()
+    {
+        return this + 1;
+    }
+
+    /// 用于申请内存,CbElement大小的空间有nMax
+    static CPlex* Create(CPlex*& pHead, UINT nMax, UINT cbElement);
+
+    /// 释放以当前对象为首地址的内存链表
+    void FreeDataChain();
+};
---
+++ src/mfc_SimpleCPlex/src/main.cpp
@@ -0,0 +1,55 @@
+﻿#include "_afxplex_.h"
+#include <iostream>
+
+struct CMyData
+{
+    int nSomeData;
+    int nSomeMoreData;
+};
+
+int main()
+{
+    CPlex* pBlocks = NULL;
+
+    /// 分配第一个内存块
+    CPlex*   pBlock1 = CPlex::Create(pBlocks, 10, sizeof(CMyData));
+    CMyData* pData1  = (CMyData*)pBlock1->data();
+
+    /// 使用第一个块的数据
+    for (int i = 0; i < 10; i++)
+    {
+        // (pData1 + i)->nSomeData = i;
+        pData1[i].nSomeData = i;
+    }
+
+    /// 分配第二个内存块
+    CPlex*   pBlock2 = CPlex::Create(pBlocks, 10, sizeof(CMyData));
+    CMyData* pData2  = (CMyData*)pBlock2->data();
+
+    /// 使用第二个块的数据
+    for (int i = 0; i < 10; i++)
+    {
+        pData2[i].nSomeData = i + 10; /// 从10开始
+    }
+
+    /// 遍历所有块的数据
+    CPlex* pCurrent   = pBlocks;
+    int    blockIndex = 0;
+    while (pCurrent != NULL)
+    {
+        CMyData* pData = (CMyData*)pCurrent->data();
+        std::cout << "块" << blockIndex << "的数据: ";
+        for (int i = 0; i < 10; i++)
+        {
+            std::cout << pData[i].nSomeData << " ";
+        }
+        std::cout << std::endl;
+
+        pCurrent = pCurrent->pNext;
+        blockIndex++;
+    }
+
+    /// 释放所有内存块
+    pBlocks->FreeDataChain();
+    return 0;
+}
