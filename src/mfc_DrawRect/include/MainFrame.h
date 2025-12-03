cea7b66121ac3e6e49220b121835267fbaa753ec
handabao <18862963156@163.com>
2025-12-03 11:01:10 +0800

    初始 MFC Rect 文档

---
+++ src/mfc_DrawRect/include/MainFrame.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "stdafx.h"
+
+class CMainFrame : public CFrameWnd
+{
+protected: /// 仅从序列化创建
+    CMainFrame();
+    DECLARE_DYNCREATE(CMainFrame)
+
+    /// 特性
+public:
+    /// 操作
+public:
+    /// 重写
+public:
+    BOOL PreCreateWindow(CREATESTRUCT& cs) override;
+
+    /// 实现
+public:
+    ~CMainFrame() override;
+#ifdef _DEBUG
+    void AssertValid() const override;
+    void Dump(CDumpContext& dc) const override;
+#endif
+
+protected: /// 控件条嵌入成员
+    CToolBar   m_wndToolBar;
+    CStatusBar m_wndStatusBar;
+
+    /// 生成的消息映射函数
+protected:
+    afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+    DECLARE_MESSAGE_MAP()
+};
---
+++ src/mfc_DrawRect/src/MainFrame.cpp
@@ -0,0 +1,78 @@
+﻿#include "MainFrame.h"
+#include "resource.h"
+
+
+IMPLEMENT_DYNCREATE(CMainFrame, CFrameWnd)
+
+BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
+ON_WM_CREATE()
+END_MESSAGE_MAP()
+
+static UINT indicators[] = {
+    ID_SEPARATOR, /// 状态行指示器
+    ID_INDICATOR_CAPS,
+    ID_INDICATOR_NUM,
+    ID_INDICATOR_SCRL,
+};
+
+
+CMainFrame::CMainFrame()
+{
+}
+
+BOOL CMainFrame::PreCreateWindow(CREATESTRUCT &cs)
+{
+    if (!CFrameWnd::PreCreateWindow(cs))
+        return FALSE;
+
+    // TODO: 在此处通过修改
+    //  CREATESTRUCT cs 来修改窗口类或样式
+
+    return TRUE;
+}
+
+CMainFrame::~CMainFrame()
+{
+}
+
+#ifdef _DEBUG
+void CMainFrame::AssertValid() const
+{
+    CFrameWnd::AssertValid();
+}
+
+void CMainFrame::Dump(CDumpContext &dc) const
+{
+    CFrameWnd::Dump(dc);
+}
+#endif //_DEBUG
+
+int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+    if (CFrameWnd::OnCreate(lpCreateStruct) == -1)
+        return -1;
+
+    if (!m_wndToolBar.CreateEx(this, TBSTYLE_FLAT,
+                               WS_CHILD | WS_VISIBLE | CBRS_TOP | CBRS_GRIPPER | CBRS_TOOLTIPS | CBRS_FLYBY |
+                                       CBRS_SIZE_DYNAMIC) ||
+        !m_wndToolBar.LoadToolBar(IDR_MAINFRAME))
+    {
+        TRACE0("未能创建工具栏\n");
+        return -1; /// 未能创建
+    }
+
+    if (!m_wndStatusBar.Create(this))
+    {
+        TRACE0("未能创建状态栏\n");
+        return -1; /// 未能创建
+    }
+    m_wndStatusBar.SetIndicators(indicators, sizeof(indicators) / sizeof(UINT));
+
+    // TODO: 如果不需要可停靠工具栏，则删除这三行
+    m_wndToolBar.EnableDocking(CBRS_ALIGN_ANY);
+    EnableDocking(CBRS_ALIGN_ANY);
+    DockControlBar(&m_wndToolBar);
+
+
+    return 0;
+}
