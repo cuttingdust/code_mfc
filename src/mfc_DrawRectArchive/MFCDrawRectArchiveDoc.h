a26bf620100128b42c140e0c8e37bf3266d4acef
handabao <18862963156@163.com>
2025-12-04 18:09:20 +0800

    MFC 文档的序列化与反序列化

---
+++ src/mfc_DrawRectArchive/MFCDrawRectArchiveDoc.h
@@ -0,0 +1,67 @@
+﻿
+// MFCDrawRectArchiveDoc.h: CMFCDrawRectArchiveDoc 类的接口
+//
+
+
+#pragma once
+
+class CRectPos : public CObject
+{
+    DECLARE_SERIAL(CRectPos)
+public:
+    int m_x, m_y, m_x1, m_y1;
+
+public:
+    CRectPos()
+    {
+    }
+    CRectPos(int x, int y, int x1, int y1);
+    virtual void Serialize(CArchive& ar);
+
+public:
+    ~CRectPos()
+    {
+    }
+};
+
+
+class CMFCDrawRectArchiveDoc : public CDocument
+{
+protected: // 仅从序列化创建
+    CMFCDrawRectArchiveDoc() noexcept;
+    DECLARE_DYNCREATE(CMFCDrawRectArchiveDoc)
+
+    // 特性
+public:
+    CTypedPtrList<CObList, CRectPos*> m_RectList;
+    void                              NewRect(int x, int y, int x1, int y1);
+    void                              DrawRect(CDC* pDC);
+    // 操作
+public:
+    // 重写
+public:
+    virtual BOOL OnNewDocument();
+    virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+    virtual void InitializeSearchContent();
+    virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+    // 实现
+public:
+    virtual ~CMFCDrawRectArchiveDoc();
+#ifdef _DEBUG
+    virtual void AssertValid() const;
+    virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+    // 生成的消息映射函数
+protected:
+    DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+    // 用于为搜索处理程序设置搜索内容的 Helper 函数
+    void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
---
+++ src/mfc_DrawRectArchive/MFCDrawRectArchiveView.h
@@ -0,0 +1,50 @@
+﻿
+// MFCDrawRectArchiveView.h: CMFCDrawRectArchiveView 类的接口
+//
+
+#pragma once
+
+
+class CMFCDrawRectArchiveView : public CView
+{
+protected: // 仅从序列化创建
+    CMFCDrawRectArchiveView() noexcept;
+    DECLARE_DYNCREATE(CMFCDrawRectArchiveView)
+
+    // 特性
+public:
+    CMFCDrawRectArchiveDoc* GetDocument() const;
+
+    // 操作
+public:
+    // 重写
+public:
+    virtual void OnDraw(CDC* pDC); // 重写以绘制该视图
+    virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+protected:
+    virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+    virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+    virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+    // 实现
+public:
+    virtual ~CMFCDrawRectArchiveView();
+#ifdef _DEBUG
+    virtual void AssertValid() const;
+    virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+    afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+    // 生成的消息映射函数
+protected:
+    DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG // MFCDrawRectArchiveView.cpp 中的调试版本
+inline CMFCDrawRectArchiveDoc* CMFCDrawRectArchiveView::GetDocument() const
+{
+    return reinterpret_cast<CMFCDrawRectArchiveDoc*>(m_pDocument);
+}
+#endif
