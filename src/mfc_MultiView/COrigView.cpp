bff9563e741311fe6c54fc0ccd50d7eefeaa7002
handabao <18862963156@163.com>
2025-12-04 11:28:25 +0800

    一档多视

---
+++ src/mfc_MultiView/COrigView.cpp
@@ -0,0 +1,70 @@
+﻿// COrigView.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "COrigView.h"
+#include "MFCMultiView.h"
+
+
+// COrigView
+
+IMPLEMENT_DYNCREATE(COrigView, CView)
+
+COrigView::COrigView()
+{
+}
+
+COrigView::~COrigView()
+{
+}
+
+BEGIN_MESSAGE_MAP(COrigView, CView)
+ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+
+// COrigView 绘图
+
+void COrigView::OnDraw(CDC* pDC)
+{
+    CMFCMultiViewDoc* pDoc = (CMFCMultiViewDoc*)GetDocument();
+    // TODO:  在此添加绘制代码
+    for (int i = 0; i < pDoc->m_RectArg.GetSize(); i++)
+    {
+        pDC->Rectangle(pDoc->m_RectArg[i]);
+    }
+}
+
+
+// COrigView 诊断
+
+#ifdef _DEBUG
+void COrigView::AssertValid() const
+{
+    CView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void COrigView::Dump(CDumpContext& dc) const
+{
+    CView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// COrigView 消息处理程序
+
+void COrigView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+    // TODO: 在此添加消息处理程序代码和/或调用默认值
+    CMFCMultiViewDoc* pDoc = (CMFCMultiViewDoc*)GetDocument();
+    int               r    = rand() % 50 + 5;
+    CRect             Ret(point.x - r, point.y - r, point.x + r, point.y + r);
+
+    pDoc->m_RectArg.Add(Ret);
+    InvalidateRect(Ret, FALSE);
+    pDoc->UpdateAllViews(this);
+
+    CView::OnLButtonDown(nFlags, point);
+}
---
+++ src/mfc_MultiView/COrigView.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "MFCMultiViewDoc.h"
+
+// COrigView 视图
+
+class COrigView : public CView
+{
+    DECLARE_DYNCREATE(COrigView)
+
+protected:
+    COrigView(); // 动态创建所使用的受保护的构造函数
+    virtual ~COrigView();
+
+public:
+    virtual void OnDraw(CDC* pDC); // 重写以绘制该视图
+#ifdef _DEBUG
+    virtual void AssertValid() const;
+#ifndef _WIN32_WCE
+    virtual void Dump(CDumpContext& dc) const;
+#endif
+#endif
+
+protected:
+    DECLARE_MESSAGE_MAP()
+public:
+    afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
