f61fe9edbc23e456689153403794c29fc358f821
handabao <18862963156@163.com>
2025-12-05 11:23:46 +0800

    初始化拼图游戏

---
+++ src/mfc_TingTuGame/OrgView.cpp
@@ -0,0 +1,51 @@
+﻿// OrgView.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "OrgView.h"
+
+
+// OrgView
+
+IMPLEMENT_DYNCREATE(OrgView, CView)
+
+OrgView::OrgView()
+{
+
+}
+
+OrgView::~OrgView()
+{
+}
+
+BEGIN_MESSAGE_MAP(OrgView, CView)
+END_MESSAGE_MAP()
+
+
+// OrgView 绘图
+
+void OrgView::OnDraw(CDC* pDC)
+{
+	CDocument* pDoc = GetDocument();
+	// TODO:  在此添加绘制代码
+}
+
+
+// OrgView 诊断
+
+#ifdef _DEBUG
+void OrgView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void OrgView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// OrgView 消息处理程序
---
+++ src/mfc_TingTuGame/TingTuGameView.cpp
@@ -0,0 +1,82 @@
+﻿
+// TingTuGameView.cpp: CTingTuGameView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "TingTuGame.h"
+#endif
+
+#include "TingTuGameDoc.h"
+#include "TingTuGameView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CTingTuGameView
+
+IMPLEMENT_DYNCREATE(CTingTuGameView, CView)
+
+BEGIN_MESSAGE_MAP(CTingTuGameView, CView)
+END_MESSAGE_MAP()
+
+// CTingTuGameView 构造/析构
+
+CTingTuGameView::CTingTuGameView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CTingTuGameView::~CTingTuGameView()
+{
+}
+
+BOOL CTingTuGameView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CTingTuGameView 绘图
+
+void CTingTuGameView::OnDraw(CDC* /*pDC*/)
+{
+	CTingTuGameDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CTingTuGameView 诊断
+
+#ifdef _DEBUG
+void CTingTuGameView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CTingTuGameView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CTingTuGameDoc* CTingTuGameView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CTingTuGameDoc)));
+	return (CTingTuGameDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CTingTuGameView 消息处理程序
